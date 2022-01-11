ad9c4a9ea10e4ecea9ef52bec021ac2c51349c96
Yordan <yordan_yordanov001@abv.bg>
2022-01-11 08:20:08 +0200

    solution

--- src/js/Application.js
+++ src/js/Application.js
@@ -11,15 +11,22 @@ export default class Application extends EventEmitter {
   constructor() {
     super();
 
-    const lyrics = ["Ah", "ha", "ha", "ha", "stayin' alive", "stayin' alive"];
-    let count = 0;
-
-    const message = document.createElement("div");
-    message.classList.add("message");
-    message.innerText = "Ah";
-
-    document.querySelector(".main").appendChild(message);
+    this._beat = new Beat();
+    this._create();
 
     this.emit(Application.events.READY);
+  }
+
+  _create() {
+    const lyrics = ["Ah", "ha", "ha", "ha", "stayin' alive", "stayin' alive"];
+    let count = 0;
+    
+    this._beat.on(Beat.events.BIT, (bit) => {
+      const message = document.createElement("div");
+      message.classList.add("message");
+      message.innerText = lyrics[count];
+      document.querySelector(".main").appendChild(message);
+      count++;
+    });
   }
 }
